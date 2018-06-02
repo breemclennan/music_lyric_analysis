211233b005478dc70ff37bf66564b36ecc2586d1
BuildTools <unconfigured@null.spigotmc.org>
2018-06-03 01:05:10 +1000

    work in progress

--- Code/Dev/02_TextAnalysis.R
+++ Code/Dev/02_TextAnalysis.R
@@ -47,6 +47,13 @@ my_kable_styling <- function(dat, caption) {
                   full_width = FALSE)
 }
 
+titles.format <- theme(plot.title = element_text(face="bold", size=13, color='grey50'),
+                       plot.subtitle = element_text(color='grey50'),
+                       axis.title = element_text(size=9, color='grey50'), 
+                       axis.text = element_text(size=9, color='grey50'),
+                       plot.margin = unit(c(0.3,0.3,0.3,0.3), "cm"))
+
+
 
 # Define a function that computes file paths relative to where root .git folder is located
 F <- is_git_root$make_fix_file() 
@@ -381,104 +388,82 @@ resLedZepKashmir$songMat
 resLedZepKashmir$repetitiveness
 
 
-#==== TODO: time duration is not available for all albums
+#======= run this for all songs...
+library(glue)
+SongSim_All_01 <- wrk.01_DataPrep_LyricsWithSpotify %>%
+  filter(style_name == "body" & text != "[Instrumental]") %>%
+  mutate(CATMusicAlbum = as.factor(CATMusicAlbum),
+         CATMusicArtist = as.factor(CATMusicArtist),
+         CATTrackName = as.factor(CATTrackName)) %>%
+  mutate(CATArtistAlbumTrack = paste(CATMusicArtist, CATMusicAlbum, CATTrackName, sep = " - ")) %>%
+  select(CATArtistAlbumTrack, text)
+
+SongList <- tapply(SongSim_All_01$text, SongSim_All_01$CATArtistAlbumTrack, matrix, byrow = TRUE)
+
+lapply(names(SongList),
+       function(x, SongList) write.table(SongList[[x]], paste((F("Data/Raw")),"/", x ,".txt", sep = ""),
+                                                    col.names = FALSE, row.names=FALSE, sep="\t", 
+                                                    quote = FALSE), SongList)
 
 
-  #mutate(CATMusicArtist = as.factor(CATMusicArtist )) %>%
-  #mutate(CATMusicAlbum = as.factor(CATMusicAlbum )) %>%
- #mutate(key = as.factor(key)) #%>%
-  #mutate(duration_ms = minutes(duration_ms)) 
+RenderSongSim <- lapply(names(SongList),
+                        function(x, SongList) songsim(path = paste((F("Data/Raw")),"/", x ,".txt", sep = ""),
+                                                      colorfulMode = TRUE,
+                                                      interactiveMode = FALSE, 
+                                                      #singleColor = "blue",
+                                                      plotOptions = c(width = 4, height = 4),
+                                                      mainTitle = x),
+                                                      SongList)
 
+#Save the base graphics from songsim to pdf
+pdf(paste((F("Data/Processed/SongSim_AllSongs.pdf"))))
+  print(
+        lapply(names(SongList),
+               function(x, SongList) songsim(path = paste((F("Data/Raw")),"/", x ,".txt", sep = ""),
+                                             colorfulMode = TRUE,
+                                             interactiveMode = FALSE, 
+                                             #singleColor = "blue",
+                                             plotOptions = c(width = 4, height = 4),
+                                             mainTitle = x), SongList)
+  )
 
+# Save PDF
+dev.off()  
+
+# Convert pdf pages to png
+library(pdftools)
+#This will save the PNGs to the work dir
+pdf_file <- F("Data/Processed/SongSim_AllSongs.pdf")
+sapply(pdf_file, function(x)
+  pdf_convert(x, format = "png", pages = NULL, filenames = NULL, dpi = 300, opw = "", upw = "", verbose = TRUE))
+
+# read in and arrange the png files -- TODO - fix this bit
+filenames <- dir(path = F("Data/Processed/") ,pattern = "SongSim_AllSongs_")
+PNGList <- list()
+for(j in 1:60) PNGList[[j]] <- readPNG(filenames[j]) #might be looking at working dir...we moved the files
+layout(matrix(1:60,nr=20,byr=TRUE))
+ for (j in 1:60) plot(PNGList[[j]])
+
+#===================
 
 ## PART A: LYRICAL ANALYSIS
 # What do the songs say?
-df <- df %>%
-  ungroup() %>%
-  mutate(TXTAllTrackLyrics = str_replace_all(TXTAllTrackLyrics, '\'', ' ')) %>%
-  mutate(nbreLines = str_count(TXTAllTrackLyrics, '<br>') + 1) %>%
-  mutate(nbreWord = str_count(TXTAllTrackLyrics, ' ') + 1)
+wrk.02_TextAnalysis_03_CommonWordsSong
 
-lineToken <- df %>%
-  unnest_tokens(line, TXTAllTrackLyrics, token = stringr::str_split, pattern = '<br>') %>% 
-  mutate(lineCount = row_number())
-
-wordToken <-  lineToken %>% 
-  unnest_tokens(word, line) %>% 
-  mutate(wordCount = row_number())
-
-countWord <- count(wordToken, word, sort=TRUE)
-countWord <- head(countWord, 100)
-empty <- data.frame(a=character(100),b=rep('|',100),c=character(100), stringsAsFactors = FALSE)
-
-data("stop_words")
-wordToken2 <- wordToken %>% 
-  anti_join(stop_words) %>%
-  arrange(wordCount)
-countWord2 <- count(wordToken2, word, sort=TRUE)
-countWord2 <- head(countWord2, 100)
 
 #plot setup
-medianWord <- median(df$nbreWord)
-uniqueWords <- wordToken2 %>% 
-  select(word) %>%
-  filter(!str_detect(word, '[0-9]')) %>%
-  group_by(word) %>% 
+uniqueWords <- wrk.02_TextAnalysis_03_CommonWordsSong %>% 
+  select(CATTrackName, word) %>%
+  filter(!str_detect(word, '[0-9]')) %>% #or we could transmute digits to words...
+  group_by(CATTrackName, word) %>% 
   filter(row_number(word) == 1) %>%
   arrange(word)
 nbreUniqWords <- nrow(uniqueWords)
 
-#distribution plot of songs by number of words
-df %>% 
-  ggplot(., aes(x=nbreWord)) +
-  geom_histogram(binwidth=10,
-                 color='white',
-                 fill='#FCCB85') +
-  geom_vline(aes(xintercept=medianWord), colour="#990000", linetype="dashed") +
-  coord_cartesian(ylim=c(0, 15)) + 
-  scale_y_continuous(breaks=seq(0, 15, 1)) +
-  scale_x_continuous(breaks=seq(0, 400, 20)) +
-  theme(panel.grid.minor = element_blank()) +
-  xlab('Total Number of Words') +
-  ylab('Nbre of Songs') +
-  labs(title='Distribution of Songs by Number of Words', 
-       subtitle='Verses repeats not included - Dashed red line is median') +
-  titles.format
-
-# Unigrams  and Word clouds 
-tab <- cbind(countWord, empty, countWord2)
-kable(tab[1:20,], format='markdown', row.names = F,
-      col.names = c('All uniGrams', 'Freq', ' ', '|', ' ', 'Cleaned uniGrams', 'Freq'))
-
-layout(matrix(c(1,2),1,2, byrow = TRUE)) #6 ROWS 2 COLS
-wordcloud(countWord$word, countWord$n, random.order=FALSE, max.words = 100, 
-          colors=brewer.pal(8, "Dark2"), use.r.layout=TRUE)
-wordcloud(countWord2$word, countWord2$n, random.order=FALSE, max.words = 100,
-          colors=brewer.pal(8, "Dark2"), use.r.layout=TRUE)
-
-#Unigrams by album
-l <- length(levels(as.factor(wordToken2$CATMusicAlbum)))
-plotList <- list()
-for(i in 1:l){
-  part <- wordToken2[wordToken2$CATMusicAlbum == levels(as.factor(wordToken2$CATMusicAlbum))[i],] %>%
-    filter(BINTrackIsInstrumental == 0) %>%
-    group_by(CATMusicAlbum) %>%
-    count(word) %>%
-    top_n(20)
-  p <- ggplot(part[1:20,], aes(reorder(word,n), n)) +
-    geom_bar(stat = "identity", fill='#FCCB85', width=0.65) +
-    #        scale_fill_discrete(drop=F) +
-    labs(y=NULL, x=NULL, title=paste('Album: ', levels(as.factor(wordToken2$CATMusicAlbum))[i], sep='')) +
-    coord_flip() +
-    titles.format +
-    theme(plot.title = element_text(size=11))
-  plotList[[i]] <- p
-}
-do.call(grid.arrange, c(plotList, ncol=3))
 
 ## BI GRAMS & TRI GRAMS
-nGram <- data_frame(text=paste(wordToken$word, collapse = ' '))
-nGramCleaned <- data_frame(text=paste(wordToken2$word, collapse = ' '))
+nGram <- data_frame(text=paste(wrk.02_TextAnalysis_03_CommonWordsSong$word, collapse = ' '))
+nGramCleaned <- data_frame(text=paste(wrk.02_TextAnalysis_03_CommonWordsSong$word, collapse = ' '))
 
 biGrams <-  nGram %>% 
   unnest_tokens(ngram, text, token = "ngrams", n = 2) %>%
